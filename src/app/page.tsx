8dab6dcf2165b87b46e298aff44befd4ead8771c
Javier Moreno <javier@pai.lat>
2024-05-30 19:37:01 -0500

    Feat: Separates icons and role component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,26 @@
-import Image, { type ImageProps } from 'next/image'
+import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+
+import { Role } from '@/app/Role/page'
 
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
+
 import {
   GitHubIcon,
   InstagramIcon,
   LinkedInIcon,
   XIcon,
 } from '@/components/SocialIcons'
+
+import { MailIcon, BriefcaseIcon, ArrowDownIcon } from '@/components/UtilIcons'
+
 import logoAirbnb from '@/images/logos/airbnb.svg'
 import logoFacebook from '@/images/logos/facebook.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoPai from '@/images/logos/pai.jpeg'
 import logoStarbucks from '@/images/logos/starbucks.svg'
 import portraitImage from '@/images/portrait.jpg'
-
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function BriefcaseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function ArrowDownIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
 
 function SocialLink({
   icon: Icon,
@@ -86,58 +34,14 @@ function SocialLink({
     </Link>
   )
 }
-interface Role {
-  company: string
-  title: string
-  logo: ImageProps['src']
-  start: string | { label: string; dateTime: string }
-  end: string | { label: string; dateTime: string }
-}
-
-function Role({ role }: { role: Role }) {
-  let startLabel =
-    typeof role.start === 'string' ? role.start : role.start.label
-  let startDate =
-    typeof role.start === 'string' ? role.start : role.start.dateTime
-
-  let endLabel = typeof role.end === 'string' ? role.end : role.end.label
-  let endDate = typeof role.end === 'string' ? role.end : role.end.dateTime
-
-  return (
-    <li className="flex gap-4">
-      <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-        <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
-      </div>
-      <dl className="flex flex-auto flex-wrap gap-x-2">
-        <dt className="sr-only">Company</dt>
-        <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-          {role.company}
-        </dd>
-        <dt className="sr-only">Role</dt>
-        <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-          {role.title}
-        </dd>
-        <dt className="sr-only">Date</dt>
-        <dd
-          className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-          aria-label={`${startLabel} until ${endLabel}`}
-        >
-          <time dateTime={startDate}>{startLabel}</time>{' '}
-          <span aria-hidden="true">—</span>{' '}
-          <time dateTime={endDate}>{endLabel}</time>
-        </dd>
-      </dl>
-    </li>
-  )
-}
 
 function Resume() {
   let resume: Array<Role> = [
     {
-      company: 'Planetaria',
-      title: 'CEO',
-      logo: logoPlanetaria,
-      start: '2019',
+      company: 'Pai Suscripciones',
+      title: 'Frontend developer',
+      logo: logoPai,
+      start: '05/2021',
       end: {
         label: 'Present',
         dateTime: new Date().getFullYear().toString(),
@@ -208,7 +112,7 @@ export default async function Home() {
               Frontend developer, amateur cyclist and enthusiastic marathon
               runner
             </h3>
-            <div className="mt-6 space-y-3 text-base text-zinc-600 dark:text-zinc-400">
+            <div className="mt-6 space-y-2 text-base text-zinc-600 dark:text-zinc-400">
               <p className="text-balance">
                 +3 years of experience building and maintaining responsive web
                 applications, from Bogotá, Colombia.
@@ -219,25 +123,32 @@ export default async function Home() {
               </p>
             </div>
 
-            <div className="mt-6 flex justify-evenly gap-4 lg:mt-12">
-              <SocialLink href="https://x.com/javandresmoreno" icon={XIcon} />
+            <div className="mt-4 flex justify-evenly gap-4 lg:mt-8">
+              <SocialLink
+                href="https://x.com/javandresmoreno"
+                icon={XIcon}
+                target="_blank"
+              />
               <SocialLink
                 href="https://instagram.com/javandresmoreno"
                 icon={InstagramIcon}
+                target="_blank"
               />
               <SocialLink
                 href="https://github.com/javandresmoreno"
                 icon={GitHubIcon}
+                target="_blank"
               />
               <SocialLink
                 href="https://www.linkedin.com/in/javandresmoreno"
                 icon={LinkedInIcon}
+                target="_blank"
               />
             </div>
             <Button
               href="mailto:hola@javandresmoreno.com"
-              variant="secondary"
-              className="group mt-6 w-full"
+              variant="primary"
+              className="group mt-8 w-full"
             >
               <MailIcon className="h-7" />
               Contact me!
@@ -245,7 +156,7 @@ export default async function Home() {
           </div>
         </div>
       </Container>
-      <Container className="mt-24 md:mt-28">
+      <Container className="md:mt-18 mt-12">
         <div className="mx-auto grid grid-cols-1 gap-y-20">
           <div className="space-y-10">
             <Resume />
